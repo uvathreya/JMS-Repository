1eacf9fd0303e45cee908a875325d2d9183ec74d
user1 <user1@LAPTOP-HC1A7IB2>
2019-07-11 14:09:53 -0500

    initial commit

---
+++ spring-jms-activeMq/src/main/java/org/learning/spring/SpringJmsActiveMqApplication.java
@@ -0,0 +1,12 @@
+package org.learning.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJmsActiveMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJmsActiveMqApplication.class, args);
+	}
+}
